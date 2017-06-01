71204ac2e41aa65703013db78479a7fb4cc0def8
RvBurik <ricardovanburik@hotmail.com>
2017-06-01 23:53:32 +0200

    bugfixes, runnable except last question

--- vragenlijst.py
+++ vragenlijst.py
@@ -21,16 +21,16 @@ def toonVragen(vragen):
         antwoord = input(vragen[i]);
         #Als een antwoord binnen de vragen mannenlijst nee is, worden er geen verdere vragen gesteld
         #en gaat de quiz verder bij de algemene vragen
-        if vragen[0] == beginVragen[0]:
+        if antwoord == "n" and i == 0 and vragen[0] == beginVragen[0]:
             print("Bedankt voor het invullen!");
-            data.open(filename, a);
+            data = open(filename, 'a');
             data.writelines("Beginvraag: Nee");
             data.close();
             sys.exit();
         
         if antwoord == "j" or antwoord == "n":
             antwoorden.append(antwoord); 
-            if antwoord == "n":
+            if antwoord == "n" and vragen[0] != beginVragen[0] and vragen[0] != eindVragen[0]:
                 break;
         else:
             print("U heeft niet het juiste antwoord  ingevuld! Automatisch antwoord 'Ja' toegevoegd!");
@@ -57,12 +57,11 @@ def antwoordenToevoegenAanBestand(m, antwoord):
 print("Welkom bij de vragenlijst!");
 print("Het invullen zal niet lang duren! Alle vragen kunnen enkel beantwoorden met j (ja) of n (nee)");
 naam = input("Vul uw naam in" + " ");
+filename = "antwoorden-" + naam + "-" + time.strftime("%Y-%m-%d_%H-%M-%S") + ".txt";
+
 
 #Toont de beginvragen en slaat deze op in de list "antwoorden"
-for j in range(len(beginVragen)):
-    print("");
-    antwoord = input(beginVragen[j]);
-    antwoorden.append(antwoord);
+toonVragen(beginVragen);
     
 #als het antwoord op de 6de vraag JA is -> dus man, start de vragenlijst van de man
 if antwoorden[5] == "j":
@@ -79,7 +78,6 @@ for k in range(len(eindVragen)):
     print("");
     antwoorden.append(input(eindVragen[k]));
 
-filename = "antwoorden-" + naam + "-" + time.strftime("%Y-%m-%d_%H-%M-%S") + ".txt";
 
 data = open(filename, 'a');
 for m in range(len(antwoorden)):
